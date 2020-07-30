ba4b0b86513b7e83169865fd903ed9424b8d3c0e
Aleksa <zabumaphu@gmail.com>
2020-07-30 17:18:54 -0400

    Data cleaning occured.

--- test.py
+++ test.py
@@ -20,6 +20,18 @@ def split_train_test(data, test_ratio):
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
 
+# Splitting Data
 housing = downloader.loading_housing_data()
 train_set, test_set = split_train_test(housing, 0.2)
 
+# Creatring Variuables
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"] = housing["population"]/housing["households"]
+corr_matrix = housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+# Cleaning Data
+housing = train_set.drop("median_house_value", axis=1)
+housing_labels = train_set["median_house_value"].copy()
+housing.dropna(subset=["total_bedrooms"])
